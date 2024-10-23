8887fabd7adac13444e967124e9c1fc6cd1c4a02
Sachin1Thapa <yousachin328gmail.com>
2024-10-23 18:34:00 +0545

    Refactor staff controller and add image upload functionality

--- src/controller/staff/staff.controller.js
+++ src/controller/staff/staff.controller.js
@@ -1,25 +1,115 @@
-//@desc Register a new Admin
-//@route POST /api/v1/admin/register
-//@access Private
-export const RegisterAdminContoller = (req, res) => {
-	res.status(201).json({
-		message: 'Admin created successfully',
-	});
-};
+import AsyncHandler from 'express-async-handler';
+import Admin from '../../model/Staff/Admin.model.js';
+import ApiError from '../../utils/ApiError.js';
+import { ApiResponse } from '../../utils/ApiResponse.js';
+import { generateAccessTokenAndRefreshToken } from '../../utils/GenerateToken.js';
+import { cookiesOptions } from '../../constant.js';
+import { uploadOnCloudinary } from '../../utils/Images/cloudinary.js';
 
-//@desc Login Admin
-//@route POST /api/v1/admin/login
-//@access Private
+//* @desc Register a new Admin
+//* @route POST /api/v1/admin/register
+//* @access Private
 
-export const LoginAdminContoller = (req, res) => {
-	res.status(200).json({
-		message: 'Admin logged in successfully',
-	});
-};
+export const RegisterAdminContoller = AsyncHandler(
+	async (req, res) => {
+		const { username, email, password } = req.body;
+		const avatarLocalPath = req.file ? req.file?.path : null;
+		if (!avatarLocalPath) {
+			throw new ApiError(400, 'Please upload an image');
+		}
+		const existedUser = await Admin.findOne({
+			$or: [{ email }, { username }],
+		});
 
-//@desc Get all Admins
-//@route GET /api/v1/admin
-//@access Private
+		if (existedUser) {
+			throw new ApiError(400, 'Admin already exist');
+		}
+
+		// upload image to cloudinary
+		const userImage = await uploadOnCloudinary(avatarLocalPath);
+		if (!userImage?.url) {
+			throw new ApiError(
+				500,
+				'Error occured while uploading image'
+			);
+		}
+
+		const User = await Admin.create({
+			username,
+			email,
+			password,
+			refreshToken: '',
+			userImage: userImage.url,
+		});
+		const curretUser = await Admin.findById(User._id).select(
+			'-password -refreshToken'
+		);
+		if (!curretUser) {
+			throw new ApiError(
+				500,
+				'Error occured while creating Admin'
+			);
+		}
+		return res
+			.status(201)
+			.json(
+				new ApiResponse(
+					201,
+					curretUser,
+					'Admin created successfully'
+				)
+			);
+	}
+);
+
+//* @desc Login Admin
+//* @route POST /api/v1/admin/login
+//* @access Private
+
+export const LoginAdminContoller = AsyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		throw new ApiError(400, 'Please provide email and password');
+	}
+
+	// check if the admin exist in the database
+	const Currentuser = await Admin.findOne({ email });
+	if (!Currentuser) {
+		throw new ApiError(404, 'Invalid credentials');
+	}
+
+	// check if the password match
+	const isPasswordValid = await Currentuser.isPasswordCorrect(
+		password
+	);
+	if (!isPasswordValid) {
+		throw new ApiError(401, 'Invalid credentials');
+	}
+
+	const { refreshToken, accessToken } =
+		await generateAccessTokenAndRefreshToken(Currentuser._id);
+
+	const loginAdminDetails = await Admin.findById(
+		Currentuser._id
+	).select('-password -refreshToken');
+
+	return res
+		.status(200)
+		.cookie('accessToken', accessToken, cookiesOptions)
+		.cookie('refreshToken', refreshToken, cookiesOptions)
+		.json(
+			new ApiResponse(
+				200,
+				loginAdminDetails,
+				'Admin logged in successfully'
+			)
+		);
+});
+
+//* @desc Get all Admins
+//* @route GET /api/v1/admin
+//* @access Private
 
 export const GetAllAdminsContoller = (req, res) => {
 	res.status(200).json({
@@ -27,9 +117,9 @@ export const GetAllAdminsContoller = (req, res) => {
 	});
 };
 
-//@desc Get a single Admin
-//@route GET /api/v1/admin/:id
-//@access Private
+//* @desc Get a single Admin
+//* @route GET /api/v1/admin/:id
+//* @access Private
 
 export const GetSingleAdminContoller = (req, res) => {
 	res.status(200).json({
@@ -37,9 +127,9 @@ export const GetSingleAdminContoller = (req, res) => {
 	});
 };
 
-//@desc Update Admin Info
-//@route PUT /api/v1/admin/:id
-//@access Private
+//* @desc Update Admin Info
+//* @route PUT /api/v1/admin/:id
+//* @access Private
 
 export const UpdateAdminContoller = (req, res) => {
 	res.status(200).json({
@@ -47,9 +137,9 @@ export const UpdateAdminContoller = (req, res) => {
 	});
 };
 
-//@desc Delete Admin Account
-//@route DELETE /api/v1/admin/:id
-//@access Private
+//* @desc Delete Admin Account
+//* @route DELETE /api/v1/admin/:id
+//* @access Private
 
 export const DeleteAdminContoller = (req, res) => {
 	res.status(200).json({
@@ -57,9 +147,9 @@ export const DeleteAdminContoller = (req, res) => {
 	});
 };
 
-//@desc Suspend Teacher Account
-//@route PUT /api/v1/admin/suspend/teacher/:id
-//@access Private
+//* @desc Suspend Teacher Account
+//* @route PUT /api/v1/admin/suspend/teacher/:id
+//* @access Private
 
 export const SuspendTeacherContoller = (req, res) => {
 	return res.status(200).json({
@@ -67,9 +157,9 @@ export const SuspendTeacherContoller = (req, res) => {
 	});
 };
 
-//@desc Unsuspend Teacher Account
-//@route PUT /api/v1/admin/unsuspend/teacher/:id
-//@access Private
+//* @desc Unsuspend Teacher Account
+//* @route PUT /api/v1/admin/unsuspend/teacher/:id
+//* @access Private
 
 export const UnsuspendTeacherContoller = (req, res) => {
 	res.status(200).json({
@@ -77,9 +167,9 @@ export const UnsuspendTeacherContoller = (req, res) => {
 	});
 };
 
-//@desc Withdraw Teacher
-//@route PUT /api/v1/admin/withdraw/teacher/:id
-//@access Private
+//* @desc Withdraw Teacher
+//* @route PUT /api/v1/admin/withdraw/teacher/:id
+//* @access Private
 
 export const WithdrawTeacherContoller = (req, res) => {
 	res.status(200).json({
@@ -87,9 +177,9 @@ export const WithdrawTeacherContoller = (req, res) => {
 	});
 };
 
-//@desc Unwithdraw Teacher
-//@route PUT /api/v1/admin/unwithdraw/teacher/:id
-//@access Private
+//* @desc Unwithdraw Teacher
+//* @route PUT /api/v1/admin/unwithdraw/teacher/:id
+//* @access Private
 
 export const UnwithdrawTeacherContoller = (req, res) => {
 	res.status(200).json({
@@ -97,9 +187,9 @@ export const UnwithdrawTeacherContoller = (req, res) => {
 	});
 };
 
-//@desc Publish Exams
-//@route PUT /api/v1/admin/publish/exams/:id
-//@access Private
+//* @desc Publish Exams
+//* @route PUT /api/v1/admin/publish/exams/:id
+//* @access Private
 
 export const PublishExamsContoller = (req, res) => {
 	res.status(200).json({
@@ -107,9 +197,9 @@ export const PublishExamsContoller = (req, res) => {
 	});
 };
 
-//@desc Unpublish Exams
-//@route PUT /api/v1/admin/unpublish/exams/:id
-//@access Private
+//* @desc Unpublish Exams
+//* @route PUT /api/v1/admin/unpublish/exams/:id
+//* @access Private
 
 export const UnpublishExamsContoller = (req, res) => {
 	res.status(200).json({
