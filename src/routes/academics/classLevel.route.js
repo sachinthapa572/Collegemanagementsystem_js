5966438d0f56c4aa60074713e76491633a17263d
Sachin1Thapa <yousachin328gmail.com>
2024-10-28 19:27:22 +0545

    Refactor class level route to use centralized routes file

---
+++ src/routes/academics/classLevel.route.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { isAuth } from '../../middlewares/isAuth.middleware.js';
+import { isAdmin } from '../../middlewares/isAdmin.middleware.js';
+import validateObjectId from '../../middlewares/validateObjectId.middleware.js';
+import {
+	createClassLevel,
+	deleteClassLevel,
+	getClassLevelById,
+	getClassLevels,
+	updateClassLevel,
+} from '../../controller/academics/classLevel.controller.js';
+
+const classLevelRouter = Router();
+
+classLevelRouter.use(isAuth, isAdmin);
+
+// create and get all academic years
+classLevelRouter
+	.route('/')
+	.post(createClassLevel)
+	.get(getClassLevels);
+
+// get, update and delete the academic year
+classLevelRouter
+	.route('/:id')
+	.get(validateObjectId, getClassLevelById)
+	.put(validateObjectId, updateClassLevel)
+	.delete(validateObjectId, deleteClassLevel);
+
+export default classLevelRouter;
